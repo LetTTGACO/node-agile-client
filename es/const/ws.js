c0c416db02993c50d25f69392bc0afb90f82558f
fangpengfei <1225751694@qq.com>
2022-06-22 23:40:42 +0800

    feat(ts): ts改造

--- es/const/ws.js
+++
@@ -1,8 +0,0 @@
-/**
- * websocket消息类型
- * @type {{RELOAD: string, OFFLINE: string}}
- */
-export const WEBSOCKET_ACTION = {
-    RELOAD: 'reload',
-    OFFLINE: 'offline',
-};
--- lib/const/ws.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * websocket消息类型
- * @type {{RELOAD: string, OFFLINE: string}}
- */
-export declare const WEBSOCKET_ACTION: {
-    RELOAD: string;
-    OFFLINE: string;
-};
